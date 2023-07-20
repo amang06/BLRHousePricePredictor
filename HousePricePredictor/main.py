a5e5e9985b55bb066bcc2073f1b13b1d3653e4c8
amang06 <aman@ssi.bz>
2023-07-20 12:26:55 +0530

    first commit

---
+++ HousePricePredictor/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open('RidgeModel.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    area = request.form.get('area')
+    print(location,bhk,bath,area)
+    input = pd.DataFrame([[location, area, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    return str(np.round(pipe.predict(input)[0] * 1e5, 2))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
